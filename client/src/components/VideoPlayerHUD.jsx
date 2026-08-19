310bc4e5c6df8c86dee3bcc553b98980ea984738
Deepak Sharma <use4random20@gmail.com>
2026-08-19 22:27:55 +0530

    Remove restrictive iframe sandbox attribute to fix Disable Sandbox popups on embed streams

--- client/src/components/VideoPlayerHUD.jsx
+++ client/src/components/VideoPlayerHUD.jsx
@@ -156,13 +156,13 @@ export default function VideoPlayerHUD({ mediaType, id, season = 1, episode = 1,
                             key={`${currentServer?.id}-${iframeSrc}`}
                             src={iframeSrc}
                             allowFullScreen
-                            allow="autoplay; encrypted-media; picture-in-picture; fullscreen"
-                            sandbox="allow-scripts allow-same-origin allow-forms allow-presentation allow-popups allow-modals"
+                            allow="autoplay; encrypted-media; picture-in-picture; fullscreen; accelerometer; gyroscope"
                             referrerPolicy="no-referrer-when-downgrade"
                             loading="eager"
                             title={title}
                             onError={handleIframeError}
                         />
+
                     )}
 
                 </div>
