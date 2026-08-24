511786ffcfc786d09edbc1dc39e9f37bdb737fd4
Deepak Sharma <use4random20@gmail.com>
2026-08-24 15:45:51 +0530

    purge-stale-bundles-and-deploy-fixed-hook-order

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    emptyOutDir: true,
     modulePreload: false,
     rollupOptions: {
       output: {
